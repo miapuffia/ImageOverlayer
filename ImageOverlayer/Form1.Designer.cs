dd8b54f10de3389a42f7ebff5414f9bdd1f28a7b
Robert Rioja <miapuffia@gmail.com>
2020-12-03 10:53:51 -0500

    Add project files.

---
+++ ImageOverlayer/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace ImageOverlayer {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			// 
+			// label1
+			// 
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(800, 450);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Left click + drag to move\r\nMiddle click to exit\r\nRight click to open file\r\nScroll" +
+    " lock to lower opacity and pass through inputs";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ ImageOverlayer/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Utilities;
+
+namespace ImageOverlayer {
+	public partial class Form1 : Form {
+        private static globalKeyboardHook gkh;
+
+        private bool overlayed = false;
+        private double opacity = 0.5;
+
+        public Form1() {
+            InitializeComponent();
+            this.TopMost = true;
+            this.KeyPreview = true;
+            this.AllowDrop = true;
+
+            gkh = new globalKeyboardHook();
+            // Don't let the Garbage Man interfere
+            GC.KeepAlive(gkh);
+
+            // These are the keys you want to listen to
+            gkh.HookedKeys.Add(Keys.Scroll);
+
+            // These are the handlers that will be called when the HookedKeys are pushed
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_LAYERED = 0x80000;
+        const int WS_EX_TRANSPARENT = 0x20;
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+        }
+
+        void gkh_KeyDown(object sender, KeyEventArgs e) {
+            // Use e.Handled = true to prevent other application recieving the keyPress
+            // Use e.handled = false to allow other application to recieve
+            e.Handled = false;
+
+            if(overlayed) {
+                this.Opacity = 1;
+                var style = GetWindowLong(this.Handle, GWL_EXSTYLE);
+                SetWindowLong(this.Handle, GWL_EXSTYLE, style | 0 | 0);
+            } else {
+                this.Opacity = opacity;
+                var style = GetWindowLong(this.Handle, GWL_EXSTYLE);
+                SetWindowLong(this.Handle, GWL_EXSTYLE, style | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+            }
+            overlayed = !overlayed;
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            // Make sure we unhook once the form closes
+            gkh.unhook();
+        }
+
+		private void label1_MouseDown(object sender, MouseEventArgs e) {
+            if(e.Button == MouseButtons.Left) {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+		private void label1_Click(object sender, EventArgs e) {
+            MouseEventArgs me = (MouseEventArgs) e;
+
+            if(me.Button == MouseButtons.Right) {
+                using(OpenFileDialog dlg = new OpenFileDialog()) {
+                    dlg.Title = "Open Image";
+
+                    if(dlg.ShowDialog() == DialogResult.OK) {
+                        var b = new Bitmap(dlg.FileName);
+
+                        pictureBox1.Image = b;
+
+                        this.Size = new Size(b.Width, b.Height);
+
+                        this.Controls.RemoveByKey("label1");
+                    }
+                }
+            } else if(me.Button == MouseButtons.Middle) {
+                Application.Exit();
+            }
+        }
+
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e) {
+            label1_MouseDown(sender, e);
+        }
+
+		private void pictureBox1_Click(object sender, EventArgs e) {
+            label1_Click(sender, e);
+        }
+	}
+}
