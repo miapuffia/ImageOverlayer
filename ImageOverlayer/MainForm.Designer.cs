cafa1f658f0e4f06c1a149497c9aca3cd4246f1b
miapuffia <miapuffia@gmail.com>
2022-01-19 22:58:09 -0500

    Made arrow keys move window Added colored border to make images edges clear Fixed image sizing issue

--- ImageOverlayer/MainForm.Designer.cs
+++ ImageOverlayer/MainForm.Designer.cs
@@ -24,19 +24,21 @@ namespace ImageOverlayer {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.label1 = new System.Windows.Forms.Label();
+			this.rainbowTimer = new System.Windows.Forms.Timer(this.components);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// pictureBox1
 			// 
-			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
 			this.pictureBox1.Name = "pictureBox1";
 			this.pictureBox1.Size = new System.Drawing.Size(800, 450);
-			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -44,21 +46,30 @@ namespace ImageOverlayer {
 			// 
 			// label1
 			// 
+			this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
 			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Margin = new System.Windows.Forms.Padding(0);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(800, 450);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Left click + drag to move\r\nMiddle click to exit\r\nRight click to open file\r\nScroll" +
-    " lock to lower opacity and pass through inputs";
+    " lock to lower opacity and pass through inputs\r\nArrow keys to move pixel by pixe" +
+    "l";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			this.label1.Click += new System.EventHandler(this.label1_Click);
 			this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+			// 
+			// rainbowTimer
+			// 
+			this.rainbowTimer.Interval = 50;
+			this.rainbowTimer.Tick += new System.EventHandler(this.rainbowTimer_Tick);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Red;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox1);
@@ -74,6 +85,7 @@ namespace ImageOverlayer {
 
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer rainbowTimer;
 	}
 }
 
--- ImageOverlayer/MainForm.cs
+++ ImageOverlayer/MainForm.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using Utilities;
+using System.Diagnostics;
+using Sc.Util.Rendering;
 
 namespace ImageOverlayer {
 	public partial class MainForm : Form {
@@ -29,9 +31,13 @@ namespace ImageOverlayer {
 
             // These are the keys you want to listen to
             gkh.HookedKeys.Add(Keys.Scroll);
+			gkh.HookedKeys.Add(Keys.Left);
+			gkh.HookedKeys.Add(Keys.Right);
+			gkh.HookedKeys.Add(Keys.Up);
+			gkh.HookedKeys.Add(Keys.Down);
 
-            // These are the handlers that will be called when the HookedKeys are pushed
-            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+			// These are the handlers that will be called when the HookedKeys are pushed
+			gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
         }
 
         public const int WM_NCLBUTTONDOWN = 0xA1;
@@ -49,25 +55,48 @@ namespace ImageOverlayer {
         const int GWL_EXSTYLE = -20;
         const int WS_EX_LAYERED = 0x80000;
         const int WS_EX_TRANSPARENT = 0x20;
-        protected override void OnLoad(EventArgs e) {
+
+		protected override void OnLoad(EventArgs e) {
             base.OnLoad(e);
         }
 
         void gkh_KeyDown(object sender, KeyEventArgs e) {
-            // Use e.Handled = true to prevent other application recieving the keyPress
-            // Use e.handled = false to allow other application to recieve
-            e.Handled = false;
+			// Use e.Handled = true to prevent other application recieving the keyPress
+			// Use e.handled = false to allow other application to recieve
+			e.Handled = true;
 
-            if(overlayed) {
-                this.Opacity = 1;
-                var style = GetWindowLong(this.Handle, GWL_EXSTYLE);
-                SetWindowLong(this.Handle, GWL_EXSTYLE, style | 0 | 0);
-            } else {
-                this.Opacity = opacity;
-                var style = GetWindowLong(this.Handle, GWL_EXSTYLE);
-                SetWindowLong(this.Handle, GWL_EXSTYLE, style | WS_EX_LAYERED | WS_EX_TRANSPARENT);
-            }
-            overlayed = !overlayed;
+			switch(e.KeyCode) {
+				case Keys.Scroll:
+					if(overlayed) {
+						this.Opacity = 1;
+						var style = GetWindowLong(this.Handle, GWL_EXSTYLE);
+						SetWindowLong(this.Handle, GWL_EXSTYLE, style | 0 | 0);
+					} else {
+						this.Opacity = opacity;
+						var style = GetWindowLong(this.Handle, GWL_EXSTYLE);
+						SetWindowLong(this.Handle, GWL_EXSTYLE, style | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+					}
+
+					overlayed = !overlayed;
+
+					break;
+				case Keys.Left:
+					this.Location = new Point(this.Location.X - 1, this.Location.Y);
+
+					break;
+				case Keys.Right:
+					this.Location = new Point(this.Location.X + 1, this.Location.Y);
+
+					break;
+				case Keys.Up:
+					this.Location = new Point(this.Location.X, this.Location.Y - 1);
+
+					break;
+				case Keys.Down:
+					this.Location = new Point(this.Location.X, this.Location.Y + 1);
+
+					break;
+			}
         }
 
         private void Form1_Closing(object sender, CancelEventArgs e) {
@@ -95,9 +124,11 @@ namespace ImageOverlayer {
 
 							pictureBox1.Image = b;
 
-							this.Size = new Size(b.Width + 2, b.Height + 2);
+							this.Size = new Size(b.Width + 10, b.Height + 10);
 
 							this.Controls.RemoveByKey("label1");
+
+							rainbowTimer.Start();
 						}
 					}
 				}
@@ -113,5 +144,11 @@ namespace ImageOverlayer {
 		private void pictureBox1_Click(object sender, EventArgs e) {
             label1_Click(sender, e);
         }
+
+		private void rainbowTimer_Tick(object sender, EventArgs e) {
+			var currentHSL = SimpleColorTransforms.RgBtoHsl(this.BackColor);
+
+			this.BackColor = SimpleColorTransforms.HsLtoRgb(currentHSL.H + 2.5, 1, 0.5);
+		}
 	}
 }
